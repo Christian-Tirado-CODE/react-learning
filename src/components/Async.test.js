c73ac91749bc1ff81814c1021705c9e571b073c3
Christian <tiradoc02@gmail.com>
2021-07-14 11:10:19 -0400

    Using mock functions for faking api request

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,19 @@ import Async from "./Async";
 
 describe("Async Component", () => {
     test("renders post if request succeeds", async () => {
+        /* WE ARE OVERWRITING  THE BUILT-IN FUNCTION WITH A DUMMY FETCH FUNCTION
+           WHERE WE SET THE ACTUAL VALUE THE PROMISE SHOULD RETURN.
+            THIS ALLOWS US TO PREVENT SENDING  UNNECESSARY REQUESTS TO THE API(IF THERE ARE DOZENS OF TESTS
+            THAT SEND REQUESTS IT CAN INCREASE THE NETWORK TRAFFIC AND IF THERE ARE ANY POST REQUESTS
+            THAT ALSO CAN MODIFY THE DATABASE UNINTENTIONALLY. THE TESTS WILL FAIL IF THE SERVER IS DOWN.
+            FOR THIS REASONS IT IS BETTER TO MAKE FAKE REQUESTS TO THE SERVER.
+            THE SERVER
+                )
+        */
+        window.fetch = jest.fn;
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id: '1', title: 'First Post'}]
+        });
         render(<Async />);
 
         const listItemsElement = await screen.findAllByRole('listitem');
