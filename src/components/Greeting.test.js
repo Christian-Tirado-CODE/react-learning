1496fed12b630eab99f2777769bab9d8c02854d3
Christian <tiradoc02@gmail.com>
2021-07-14 09:29:52 -0400

    The three A's in Testing

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,12 @@
+/*
+
+The three A's in Testing:
+
+    1. Arrange - Setup the test data, conditions and environment.
+
+    2. Act - Run logic that should be tested(execute functions)
+
+    3. Assert - Compare execution results with expected results.
+
+*/
+
